6055615eba53c628624c3f69801061f739eb7dc0
GatereKinyanjui <gaterekinyanjui@gmail.com>
2023-02-24 16:30:21 +0300

    standardized design layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import AdminSignIn from "./components/AdminSignIn";
+import Header from "./components/Header";
 
 const App = () => {
-  return <AdminSignIn />;
+  return (
+    <div className="App">
+      <Header />
+      <AdminSignIn />
+    </div>
+  );
 };
 
 export default App;
--- src/components/AdminSignIn/AdminSignIn.tsx
+++ src/components/AdminSignIn/AdminSignIn.tsx
@@ -1,36 +1,33 @@
-import "./style.module.css";
+import styles from "./style.module.css";
 import PbLogo from "../.././assets/pb-logo.svg";
 
 const AdminSignIn = () => {
   return (
-    <div className="wrapper">
-      <div className="logo">
+    <div className={styles.wrapper}>
+      <div className={styles.logo}>
         <img src={PbLogo} alt="" />
         <span className="text">
           Pocket<strong>Base</strong>
         </span>
       </div>
 
-      <form action="" className="formContainer">
+      <form action="" className={styles.formContainer}>
         <h4>Admin Sign In</h4>
 
-        <div className="inputRequired">
-          <label htmlFor="">
-            <input type="text" className="email" placeholder="EMAIL" />
-          </label>
+        <div className={styles.inputRequired}>
+          <label htmlFor="">Email</label>
+          <input type="email" className="email" />
         </div>
 
-        <div className="inputRequired">
-          <label htmlFor="">
-            <input type="text" className="password" placeholder="Password" />
-          </label>
+        <div className={styles.inputRequired}>
+          <label htmlFor="">Password</label>
+          <input type="password" className="password" />
+          <div className="help">
+            <a href="#">Forgotten Password?</a>
+          </div>
         </div>
 
-        <div className="help">
-          <a href="#">Forgotten Password?</a>
-        </div>
-
-        <button className="login" type="submit">
+        <button type="submit">
           <span>Login &#8594;</span>
         </button>
       </form>
